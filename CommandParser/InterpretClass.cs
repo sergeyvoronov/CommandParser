389aad6cc25a5f127379d7f2554376c3e3a3de95
update rails --no-ri --no-rdoc <sergey.voronov@gmail.com>
2015-09-20 16:35:38 +0300

    added tests

---
+++ CommandParser/InterpretClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandParser
+{
+    public class InterpretClass
+    {
+        public string interpret(string[] commands, out bool exit)
+        {
+            exit = false;
+            string result = "";
+            if (commands.Length == 0) result += "CommandParser.exe [/?] [/help] [-help] [-k key value] [-ping] [-print <print a value>] \n";
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string[] c = commands[i].Split(' ');
+                if (c[0] != "")
+                {
+                    switch (c[0])
+                    {
+                        case "?":
+                        case "help":
+                            if (i == 0)
+                                result += "CommandParser.exe [/?] [/help] [-help] [-k key value] [-ping] [-print <print a value>] \n";
+                            break;
+                        case "ping":
+                            result += "Pinging ... \n";
+                            break;
+                        case "exit":
+                            exit = true;
+                            break;
+                        case "print":
+                            if (c.Length > 1)
+                            {
+                                string message = String.Empty;
+                                for (int k = 1; k < c.Length; k++)
+                                {
+                                    message += String.Format("{0} ", c[k]);
+                                }
+                                result += String.Format("Printing message {0}\n", message);
+                            }
+                            else
+                                result += "Message can't be empty \n";
+                            break;
+                        case "k":
+                            for (int l = 1; l < c.Length; l += 2)
+                            {
+                                string key = c[l];
+                                string value = (l + 1 < c.Length) ? c[l + 1] : "null";
+                                result += String.Format("key {0} - value {1} \n", key, value);
+                            }
+                            break;
+                        default:
+                            result += String.Format("Command <{0}> is not supported, use CommandParser.exe /? to see set of allowed commands \n", c[0]);
+                            break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        //приводим массив задданых аргументов в массив, в котором каждый элемент содержит одну комманду.
+
+        public string[] prepareArraysCommandsFromArgs(string[] args)
+        {
+            string result = string.Join(" ", args);
+            return result.Split(new Char[] { '/', '-' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ CommandParserTest/CommandParserTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CommandParser;
+
+namespace CommandParserTest
+{
+    public class CommandParserTest
+    {
+        [Test]
+        public void CommandParserEmptyArgsTest()
+        {
+            string result = "CommandParser.exe [/?] [/help] [-help] [-k key value] [-ping] [-print <print a value>] \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserPingArgTest()
+        {
+            string result = "Pinging ... \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { "ping" };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserPrintArgTest()
+        {
+            string result = "Printing message hello world \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] {"print hello world"};
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserUnsupportedArgTest()
+        {
+            string result = "Command <bye> is not supported, use CommandParser.exe /? to see set of allowed commands \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { "bye" };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserKArgTest()
+        {
+            string result = "key key1 - value value1 \nkey key2 - value value2 \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { "k key1 value1 key2 value2" };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserKEmptyValueArgTest()
+        {
+            string result = "key key1 - value value1 \nkey key2 - value null \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { "k key1 value1 key2" };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserHelpIsNotFirstArgTest()
+        {
+            string result = "Pinging ... \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { "ping -help" };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserHelpArgTest()
+        {
+            string result = "CommandParser.exe [/?] [/help] [-help] [-k key value] [-ping] [-print <print a value>] \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { "help" };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+        [Test]
+        public void CommandParserHelpSignArgTest()
+        {
+            string result = "CommandParser.exe [/?] [/help] [-help] [-k key value] [-ping] [-print <print a value>] \n";
+            InterpretClass ic = new InterpretClass();
+            String[] arg = new String[] { "?" };
+            bool exit = false;
+            Assert.AreEqual(ic.interpret(arg, out exit), result);
+        }
+
+    }
+}
